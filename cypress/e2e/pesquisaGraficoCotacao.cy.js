79a28a5be533274802600dea1e50fa6e6911a5c6
Daniela Tranchezzi <danitranchezzi@outlook.com>
2025-06-25 11:24:22 -0300

    Corrigir descrição

--- cypress/e2e/pesquisaGraficoCotacao.cy.js
+++ cypress/e2e/pesquisaGraficoCotacao.cy.js
@@ -1,5 +1,5 @@
 describe('Validar pesquisa por data no gráfico da cotação', () => {
-  it('selecione data inicial e data final e valid retorno no gráfico', () => {
+  it('selecionar data inicial e final e validar retorno no gráfico', () => {
     cy.visit('https://galp.com/corp/pt/investidores/informacao-ao-acionista/acao-da-galp/desempenho-da-acao');
     cy.get(h2).contains('Gráfico da cotação');
     cy.get(p).contains('O gráfico da cotação é uma ferramenta interativa que permite observar e analisar o desempenho da ação Galp relativamente a índices e a empresas comparáveis.');
